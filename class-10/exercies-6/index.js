1be95a6f10173fc853416bba4b2138340b38de63
noeli2420 <cruznoeli24@g-apps.monett.k12.mo.us>
2022-09-27 14:16:14 -0500

    finish exercise 1- 14

---
+++ class-10/exercies-6/index.js
@@ -0,0 +1,18 @@
+let firstName; 
+firstName = "john"; 
+console.log(firstName); 
+
+let lastName; 
+lastName = "smith"; 
+console.log(lastName); 
+
+let favoriteNumber; 
+favoriteNumber =22; 
+console.log(favoriteNumber); 
+
+let person = { 
+    firstName,
+    lastName, 
+    age: "18", 
+}
+console.log(person)
